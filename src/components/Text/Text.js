9c7d116edd7f24e4d934624d86e2a76e32df5ac7
Adrian <adrian.naranjo@live.u-tad.com>
2019-07-06 12:16:29 +0200

    Mejora text añadiendo como variant los distintos tipos de texto

--- src/components/Text/Text.js
+++ src/components/Text/Text.js
@@ -1,17 +1,58 @@
 import React from "react";
 
 const Text = props => {
-  return (
-    <p
-      style={{
-        fontWeight: props.weight ? "700" : "400",
-        fontSize: props.size,
-        letterSpacing: props.spacing
-      }}
-    >
-      {props.children}
-    </p>
-  );
+  let content;
+
+  let style = {
+    fontWeight: props.weight ? "700" : "400",
+    fontSize: props.size,
+    letterSpacing: props.spacing
+  };
+  switch (props.variant) {
+    case "h1":
+      style.fontWeight = "700";
+      style.letterSpacing = "0";
+      style.fontSize = "56px";
+      style.color = "#212121";
+      content = <h1 style={style}>{props.children}</h1>;
+      break;
+    case "h2":
+      style.fontWeight = "400";
+      style.letterSpacing = "0";
+      style.fontSize = "48px";
+      style.color = "#212121";
+      content = <h2 style={style}>{props.children}</h2>;
+      break;
+    case "p1":
+      style.fontWeight = "700";
+      style.letterSpacing = "4px";
+      style.fontSize = "20px";
+      style.color = "#000000";
+      content = <p style={style}>{props.children}</p>;
+      break;
+    case "p2":
+      style.fontWeight = "400";
+      style.letterSpacing = "0";
+      style.fontSize = "20px";
+      style.color = "#000000";
+      content = <p style={style}>{props.children}</p>;
+      break;
+    case "p3":
+      style.fontWeight = "400";
+      style.letterSpacing = "0";
+      style.fontSize = "16px";
+      style.color = "#000000";
+      content = <p style={style}>{props.children}</p>;
+      break;
+    case "customize":
+      style.color = "#000000";
+      content = <p style={style}>{props.children}</p>;
+      break;
+    default:
+      content = <p style={style}>{props.children}</p>;
+      break;
+  }
+  return { content };
 };
 
 export default Text;
