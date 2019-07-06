c3d1756e07c96c5f55111c465edce55966485ddf
Adrian <adrian.naranjo@live.u-tad.com>
2019-07-06 12:45:37 +0200

    Creacion componente HeaderCard

---
+++ src/components/Molecules/Card/HeaderCard/HeaderCard.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Button from "components/Atoms/Button/Button";
+import Text from "components/Atoms/Text/Text";
+
+const HeaderCard = props => {
+  return (
+    <div className="headerCard">
+      <div className="textHeadercard">
+        <Text weight fontSize="14px">
+          Camarero
+        </Text>
+        <Text fontSize="12px">Marriot Dubai</Text>
+      </div>
+      <Button
+        type="image"
+        width="85px"
+        height="37px"
+        bg="Boton_DUBAI.jpg"
+        isBold
+        letterSpacing="2.4px"
+      >
+        DUBAI
+      </Button>
+    </div>
+  );
+};
+
+export default HeaderCard;
