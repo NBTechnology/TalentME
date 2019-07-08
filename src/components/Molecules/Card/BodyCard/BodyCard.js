0914697fb9454006c3cee4f01e0f9435f3e7bcbf
Adrian <adrian.naranjo@live.u-tad.com>
2019-07-08 21:49:09 +0200

    BodyCard componente creado

---
+++ src/components/Molecules/Card/BodyCard/BodyCard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Text from 'components/Atoms/Text/Text';
+
+import "./BodyCard.css";
+
+const BodyCard = props => {
+
+
+    return (
+        <div className="bodyCardOffer">
+            <Text variant="p3">Idealmente gente joven con ganas de una experiencia única en el mundo. Inglés fluido, con muchas corvas y que las tengan hidratadas para un correcto bien estar.</Text>
+        </div>
+    );
+};
+
+export default BodyCard;
