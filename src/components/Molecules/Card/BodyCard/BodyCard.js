05c7c166e91970524331f2fdb9bbf6d33018ef8b
Adrian <adrian.naranjo@live.u-tad.com>
2019-07-28 14:33:18 +0200

    Card diferenciada por variant

--- src/components/Molecules/Card/BodyCard/BodyCard.js
+++ src/components/Molecules/Card/BodyCard/BodyCard.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import Text from 'components/Atoms/Text/Text';
+import React from "react";
+import Text from "components/Atoms/Text/Text";
 
 import "./BodyCard.css";
 
 const BodyCard = props => {
-
-
-    return (
-        <div className="bodyCardOffer" style={{flex: "1 1 auto"}}>
-            <Text variant="p3" padding="15.5px 0px 0px 0px">Idealmente gente joven con ganas de una experiencia única en el mundo. Inglés fluido, con muchas corvas y que las tengan hidratadas para un correcto bien estar.</Text>
-        </div>
-    );
+  return (
+    <div className="bodyCardOffer" style={{ flex: "1 1 auto" }}>
+      <Text variant="p3" padding="15.5px 0px 0px 0px">
+        {props.text}
+      </Text>
+    </div>
+  );
 };
 
 export default BodyCard;
--- src/components/Molecules/Card/HeaderCard/HeaderCard.js
+++ src/components/Molecules/Card/HeaderCard/HeaderCard.js
@@ -44,8 +44,34 @@ const HeaderCard = props => {
         </div>
       );
       break;
-
     default:
+      content = (
+        <div className="headerCardOffer">
+          <div className="headerCardContentOffer">
+            <div>
+              <Text weight fontSize={props.sizeTitle}>
+                {props.title}
+              </Text>
+              <Text fontSize={props.sizeSubtitle} padding="8px 0px 0px 0px">
+                {props.subtitle}
+              </Text>
+            </div>
+            <Button
+              variant="image"
+              isGradient
+              width={props.widthButton}
+              height={props.heightButton}
+              bg={props.imageButton}
+              isBold
+              letterSpacing={props.letterSpacingButton}
+              fontSize={props.fontSizeButton}
+            >
+              {props.labelButton}
+            </Button>
+          </div>
+          <hr />
+        </div>
+      );
       break;
   }
 
