4629300a0c7f98ec66d198e84b3ba56c9a9daa6d
Adrian <adrian.naranjo@live.u-tad.com>
2019-07-29 16:52:40 +0200

    Reestructuración de componentes de Stepper

--- src/components/Organisms/EasySteps/EasySteps.js
+++ src/components/Organisms/EasySteps/EasySteps.js
@@ -1,26 +1,52 @@
 import React from "react";
-import Stepper from "components/Organisms/Stepper/Stepper"
+import Stepper from "components/Molecules/Stepper";
 import Text from "components/Atoms/Text/Text";
 import Button from "components/Atoms/Button/Button";
 import Imagen from "components/Atoms/Imagen/Imagen";
 
 const EasySteps = props => {
-    const contain =
-        <div style={{ textAlign: "center" }}>
-            <Text variant="h2" weight>4 sencillos pasos</Text>
-            <div style={{width:"60%", marginLeft:"20%"}}>
-                <Stepper type={props.type} data={props.data} />
-            </div>
-            <div style={{ textAlign: "center", display: "flex", justifyContent: "center", zIndex: "2", marginTop: "-70px" }}>
-                <Imagen style={{ zIndex: "2", opacity: "0.65", filter: "alpha(opacity=50)" }} url="Home_4Sencillospasos.jpg" height="216px" width="100%" isBackground>
-                    <div style={{ textAlign: "center", display: "flex", justifyContent: "center", zIndex: "2", marginTop: "124px" }}>
-                        <Button style={{ zIndex: "1" }} width="188px" height="51px">INSCRIBIRSE</Button>
-                    </div>
-                </Imagen>
-
-            </div>
-        </div>
-    return contain;
-}
+  const contain = (
+    <div style={{ textAlign: "center", top: "48px", position: "relative" }}>
+      <Text variant="h2" weight>
+        4 sencillos pasos
+      </Text>
+      <div style={{ width: "60%", marginLeft: "20%" }}>
+        <Stepper type={props.type} data={props.data} />
+      </div>
+      <div
+        style={{
+          textAlign: "center",
+          display: "flex",
+          justifyContent: "center",
+          zIndex: "2",
+          marginTop: "-70px"
+        }}
+      >
+        <Imagen
+          style={{ zIndex: "2", opacity: "0.65", filter: "alpha(opacity=50)" }}
+          url="Home_4Sencillospasos.jpg"
+          height="216px"
+          width="100%"
+          isBackground
+        >
+          <div
+            style={{
+              textAlign: "center",
+              display: "flex",
+              justifyContent: "center",
+              zIndex: "2",
+              marginTop: "124px"
+            }}
+          >
+            <Button style={{ zIndex: "1" }} width="188px" height="51px">
+              INSCRIBIRSE
+            </Button>
+          </div>
+        </Imagen>
+      </div>
+    </div>
+  );
+  return contain;
+};
 
 export default EasySteps;
