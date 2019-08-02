47df7407c2c776ee049ccf84797c923c882b121b
Sergio Redondo <redondomontes.sergio@gmail.com>
2019-08-02 20:37:05 +0200

    Creada la página ConctacUs, falta la columna de la derecha y comprobar que los campos están rellenos.

--- src/containers/ContactUs/ContactUs.js
+++ src/containers/ContactUs/ContactUs.js
@@ -5,16 +5,137 @@ import Button from "components/Atoms/Button/Button";
 import Imagen from "components/Atoms/Imagen/Imagen";
 import Text from "components/Atoms/Text/Text";
 import Footer from "components/Organisms/Footer/Footer";
+import Input from "components/Atoms/Input/Input";
+import { Checkbox } from '@material-ui/core';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+
+
+
 
 class ContactUs extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            name:"",
+            company:"",
+            phone:"",
+            checkPolity: false
+        };
     }
+
+    inputHandler = event => {
+        this.setState({
+          [event.target.name]: event.target.value
+        });
+      };
+
+    handleChange = name => event => {
+        this.setState({[name]: event.target.checked });
+    };
+
     render() {
         return (
             <div>
                 <NavBar />
                 <Header image="Contacto_Header.jpg" gradient="header-white" backgroundPosition = "bottom" />
+                <div style={{ textAlign: "center", marginTop: "42px" }}>
+                    <Text variant="h2">
+                        ¿En qué podemos ayudar?
+                    </Text>
+                </div>
+                <div 
+                    style={{
+                        width:"40%",
+                        marginLeft:"146px"
+                }}>
+                    <div 
+                    style={{ 
+                        background: "#F2F2F2", 
+                        borderRadius: "4px", 
+                        width:"100%", 
+                        height:"80px", 
+                        display:"flex", 
+                        justifyContent:"center", 
+                        alignItems:"center"
+                    }}>
+                        <Text variant="customize" fontSize="32px">
+                            Contacto profesional
+                        </Text>
+                    </div>
+                    <Input
+                        fullWidth
+                        id="idName"
+                        label="Nombre"
+                        margin="normal"
+                        maxLength="100"
+                        name="name"
+                        onChange={this.inputHandler}
+                        required
+                        type="text"
+                        value={this.state.name}
+                        variant="outlined"
+                    />
+                    <Input
+                        fullWidth
+                        id="idCompany"
+                        label="Compañía"
+                        margin="normal"
+                        maxLength="100"
+                        name="company"
+                        onChange={this.inputHandler}
+                        required
+                        type="text"
+                        value={this.state.company}
+                        variant="outlined"
+                    />
+                    <Input
+                        fullWidth
+                        id="idPhone"
+                        label="Teléfono"
+                        margin="normal"
+                        maxLength="100"
+                        name="phone"
+                        onChange={this.inputHandler}
+                        required
+                        type="text"
+                        value={this.state.phone}
+                        variant="outlined"
+                    />
+
+                    <Text variant="p3" margin="16px 0 8px 0">
+                        Mensaje
+                    </Text>
+                    <textarea
+                        style={{
+                            border: "1px solid #979797",
+                            borderRadius: "4px",
+                            width: "60%",
+                            minHeight: "150px"
+                        }}
+                    />
+                    <div style={{display:"flex", alignItems:"center", justifyContent:"flex-end"}}>
+                    <Checkbox
+                        checked={this.state.checkPolity}
+                        onChange={this.handleChange('checkPolity')}
+                        color="default" 
+                        value="checkPolity"
+                        />
+                    <Text variant="customize" fontSize="12px">Acepto la politica de privacidad</Text>
+                    </div>
+                    <div style={{display:"flex", alignItems:"center", justifyContent:"flex-end"}}>
+                        <Button
+                        variant="primary"
+                        width="127px"
+                        height="38px"
+                        fontSize="10px"
+                        isBold
+                        letterSpacing="2px"
+                        >
+                        ENVIAR
+                        </Button>
+                    </div>
+                </div>
+                <Footer />
             </div>
         )
     }
