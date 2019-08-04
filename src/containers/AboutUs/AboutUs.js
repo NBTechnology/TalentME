e22f0756cdef8ec9a91cfb6d240d62ba472d289d
Sergio Redondo <redondomontes.sergio@gmail.com>
2019-08-04 17:26:18 +0200

    Added colaboradores Esta parte tiene un problema y es que al ser imagenes de distintos tamaños podría descuadrarse todo al hacerlo responsive

--- src/containers/AboutUs/AboutUs.js
+++ src/containers/AboutUs/AboutUs.js
@@ -4,6 +4,7 @@ import Header from "components/Molecules/Header/Header";
 import Text from "components/Atoms/Text/Text";
 import Facilities from "components/Organisms/Facilities/Facilities";
 import Footer from "components/Organisms/Footer/Footer";
+import Imagen from "components/Atoms/Imagen/Imagen";
 
 class AboutUs extends Component {
     constructor(props) {
@@ -24,6 +25,39 @@ class AboutUs extends Component {
                     </Text>
             </div>
             <Facilities variant="home" />
+            <div style={{height:"628px", background:"#F2F2F2"}}></div>
+            <div>
+                <Text variant="h2" color="#212121" textAlign="center">
+                    Colaboradores
+                </Text>
+                <Text variant="custom" color="#000000" fontSize="22px" lineHeight="26px" textAlign="center">
+                    Los hoteles con los que trabajamos tienen mínimo 5 estrellas
+                </Text>
+                <div style={{width:"80%", marginLeft:"10%", marginTop:"48px", marginBottom:"64px"}}>
+                    <div style={{display:"flex", justifyContent:"space-between"}}>
+                        <div style={{width:"30%", display:"flex", justifyContent:"center", alignItems:"center"}}>
+                        <Imagen style={{width:"30%"}} url="Collaborators/kempinski.png" width="290px" height="114px"></Imagen>
+                        </div>
+                        <div style={{width:"30%", display:"flex", justifyContent:"center", alignItems:"center"}}>
+                          <Imagen style={{width:"30%"}} url="Collaborators/jumeirah.png" width="290px" height="160px"></Imagen>
+                        </div>
+                        <div style={{width:"30%", display:"flex", justifyContent:"center", alignItems:"center"}}>
+                            <Imagen style={{width:"30%"}} url="Collaborators/marriot.png" width="290px" height="150px"></Imagen>
+                        </div>
+                    </div>
+                    <div style={{display:"flex", justifyContent:"space-between"}}>
+                        <div style={{width:"30%", display:"flex", justifyContent:"center", alignItems:"center"}}>
+                            <Imagen style={{width:"30%"}} url="Collaborators/the_ritz.png" width="204px" height="142px"></Imagen>
+                        </div>
+                        <div style={{width:"30%", display:"flex", justifyContent:"center", alignItems:"center"}}>
+                            <Imagen style={{width:"30%"}} url="Collaborators/atlantis.jpg" width="325px" height="81px"></Imagen>
+                        </div>
+                        <div style={{width:"30%", display:"flex", justifyContent:"center", alignItems:"center"}}>
+                            <Imagen style={{width:"30%"}} url="Collaborators/Sheraton_Hotel_Dubai_logo.png" width="170px" height="142px"></Imagen>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <Footer></Footer>
             </div>
         )
