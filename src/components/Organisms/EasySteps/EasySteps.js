ca8c321f190095ec6579b6c37499fb11c6429034
Adrian <adriannaranjosanchez@gmail.com>
2019-08-04 18:09:21 +0200

    easysteps responsive

--- src/components/Organisms/EasySteps/EasySteps.js
+++ src/components/Organisms/EasySteps/EasySteps.js
@@ -3,25 +3,38 @@ import Stepper from "components/Molecules/Stepper";
 import Text from "components/Atoms/Text/Text";
 import Button from "components/Atoms/Button/Button";
 import Imagen from "components/Atoms/Imagen/Imagen";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles(theme => ({
+  stepper: props => ({
+    width: "70%",
+    marginLeft: "15%"
+  }),
+  imageBackground: props => ({
+    textAlign: "center",
+    display: "flex",
+    justifyContent: "center",
+    zIndex: "2",
+    [theme.breakpoints.up("md")]: {
+      marginTop: "-70px"
+    },
+    [theme.breakpoints.down("md")]: {
+      marginTop: "-92px"
+    }
+  })
+}));
 
 const EasySteps = props => {
+  const classes = useStyles(props);
   const contain = (
     <div style={{ textAlign: "center", top: "48px", position: "relative" }}>
       <Text variant="h2" weight>
         4 sencillos pasos
       </Text>
-      <div style={{ width: "60%", marginLeft: "20%" }}>
+      <div className={classes.stepper}>
         <Stepper type={props.type} data={props.data} />
       </div>
-      <div
-        style={{
-          textAlign: "center",
-          display: "flex",
-          justifyContent: "center",
-          zIndex: "2",
-          marginTop: "-70px"
-        }}
-      >
+      <div className={classes.imageBackground}>
         <Imagen
           style={{ zIndex: "2", opacity: "0.65", filter: "alpha(opacity=50)" }}
           url="Home_4Sencillospasos.jpg"
