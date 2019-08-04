60089c3735d96c609b230198bb31194898fce350
Adrian <adriannaranjosanchez@gmail.com>
2019-08-04 22:45:53 +0200

    Requisistes responsive

--- src/components/Organisms/Requisites/Requisites.js
+++ src/components/Organisms/Requisites/Requisites.js
@@ -1,42 +1,29 @@
 import React from "react";
 import Card from "components/Organisms/Card/Card";
 import Text from "components/Atoms/Text/Text";
+import { Grid } from "@material-ui/core";
 
 const Requisites = props => {
   const contentRequisites = props.data.map(requisite => {
     return (
-      <Card
-        variant="cardRequisite"
-        key={requisite.id}
-        title={requisite.title}
-        url={requisite.url}
-        text={requisite.text}
-      />
+      <Grid item xs={12} md={4}>
+        <Card
+          variant="cardRequisite"
+          key={requisite.id}
+          title={requisite.title}
+          url={requisite.url}
+          text={requisite.text}
+        />
+      </Grid>
     );
   });
   return (
-    <div
-      style={{
-        position: "relative",
-        top: "48px",
-        width: "80%",
-        marginLeft: "10%",
-        marginBottom: "48px"
-      }}
-    >
-      <div style={{ textAlign: "center" }}>
+    <Grid container justify="center">
+      <Grid item xs={12} style={{ textAlign: "center" }}>
         <Text variant="h2">¿Qué requisitos debes cumplir?</Text>
-      </div>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "space-around"
-        }}
-      >
-        {contentRequisites}
-      </div>
-    </div>
+      </Grid>
+      {contentRequisites}
+    </Grid>
   );
 };
 
