9d825d3d4ed974deb9697db24904321a19d0035f
Sergio Redondo <redondomontes.sergio@gmail.com>
2019-08-04 16:46:48 +0200

    Added AboutUs.js y imagenes necesarias Eliminadas imagenes on el formato incorrecto, creada la ruta en el fichero app.js para que podamos acceder manualmente a la ruta /about-us

---
+++ src/containers/AboutUs/AboutUs.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import NavBar from "components/Organisms/NavBar/NavBar";
+import Header from "components/Molecules/Header/Header";
+import Text from "components/Atoms/Text/Text";
+import Footer from "components/Organisms/Footer/Footer";
+
+class AboutUs extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+
+    render() {
+        return (
+            <div>
+                <NavBar />
+                <Header image="AboutUs_Header.jpeg" gradient="header-white"  />
+                <div style={{width:"60%", marginLeft:"20%", marginTop:"39px"}}>
+                    <Text variant="custom" fontSize="22px" lineHeight="25px" textAlign="center">
+                        Talent Me es una empresa española de carácter internacional que ofrece la 
+                        oportunidad de trabajar en Oriente Medio en el sector hospitality a jóvenes 
+                        que buscan un nuevo reto en su carrera profesional.
+                    </Text>
+            </div>
+            <div style={{marginTop:"45px"}}></div>
+            <Footer></Footer>
+            </div>
+        )
+    }
+}
+
+export default AboutUs;
