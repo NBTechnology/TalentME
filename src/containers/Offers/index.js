df2d2dc9610401b0af8b5c9a89644099b5293536
Sergio Redondo <redondomontes.sergio@gmail.com>
2019-08-08 17:42:00 +0200

    Finish Offers

--- src/containers/Offers/index.js
+++ src/containers/Offers/index.js
@@ -4,9 +4,11 @@ import Header from "components/Molecules/Header/Header";
 import Select from "components/Atoms/Select/Select";
 import Footer from "components/Organisms/Footer/Footer";
 import { Grid } from "@material-ui/core";
+import Offers from "components/Organisms/Offers/Offers";
+import Facilities from "components/Organisms/Facilities/Facilities";
 
 
-class Offers extends Component {
+class OffersComponent extends Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -59,37 +61,37 @@ class Offers extends Component {
             offers: [
                 {
                   id: "offer0",
-                  title: "Camarero",
+                  position: "Camarero",
                   subTitle: "Marriot Dubai",
                   image: "Boton_DOHA.jpg",
-                  label: "DUBAI",
+                  place: "DUBAI",
                   infoOffer:
                     "Idealmente gente joven con ganas de una experiencia única en el mundo. Inglés fluido, con muchas corvas y que las tengan hidratadas para un correcto bien estar."
                 },
                 {
                   id: "offer1",
-                  title: "Camarero",
+                  position: "Camarero",
                   subTitle: "Marriot Dubai",
                   image: "Boton_DOHA.jpg",
-                  label: "DUBAI",
+                  place: "DUBAI",
                   infoOffer:
                     "Idealmente gente joven con ganas de una experiencia única en el mundo. Inglés fluido, con muchas corvas y que las tengan hidratadas para un correcto bien estar."
                 },
                 {
                   id: "offer2",
-                  title: "Camarero",
+                  position: "Camarero",
                   subTitle: "Marriot Dubai",
                   image: "Boton_DOHA.jpg",
-                  label: "DUBAI",
+                  place: "DUBAI",
                   infoOffer:
                     "Idealmente gente joven con ganas de una experiencia única en el mundo. Inglés fluido, con muchas corvas y que las tengan hidratadas para un correcto bien estar."
                 },
                 {
                   id: "offer3",
-                  title: "Camarero",
+                  position: "Camarero",
                   subTitle: "Marriot Dubai",
                   image: "Boton_DOHA.jpg",
-                  label: "DUBAI",
+                  place: "DUBAI",
                   infoOffer:
                     "Idealmente gente joven con ganas de una experiencia única en el mundo. Inglés fluido, con muchas corvas y que las tengan hidratadas para un correcto bien estar."
                 }
@@ -105,44 +107,54 @@ class Offers extends Component {
         return (
             <Grid container component="main" justify="center">
                 <NavBar></NavBar>
-                <Grid item xs={12} md={10}  style={{
-                    marginTop:"154px",
-                    
-                }}>
-                    <Grid container spacing={2} style={{
-                        background:"#FFFFFF",
-                        boxShadow: "0 2px 4px 0 rgba(0,0,0,0.30)",
-                        borderRadius: "4px"
-                    }}>
-                        <Grid item  xs={12} md={6}>
-                            <Select
-                                fullWidth
-                                variant="outlined"
-                                idInput="idPosition"
-                                labelInput="Cualquier posición"
-                                value={this.state.positionOffer}
-                                handleChange={this.selectHandler}
-                                name="positionOffer"
-                                items={this.state.itemsPosition}
-                            />
+                <Grid item xs={12} style={{background:"#F2F2F2", paddingBottom:"26px"}}>
+                    <Grid container justify="center">
+                        <Grid item xs={12} md={10}  style={{
+                            marginTop:"154px"
+                        }}>
+                            <Grid container spacing={2} style={{
+                                background:"#FFFFFF",
+                                boxShadow: "0 2px 4px 0 rgba(0,0,0,0.30)",
+                                borderRadius: "4px"
+                            }}>
+                                <Grid item  xs={12} md={6}>
+                                    <Select
+                                        fullWidth
+                                        variant="outlined"
+                                        idInput="idPosition"
+                                        labelInput="Cualquier posición"
+                                        value={this.state.positionOffer}
+                                        handleChange={this.selectHandler}
+                                        name="positionOffer"
+                                        items={this.state.itemsPosition}
+                                    />
+                                </Grid>
+                                <Grid item  xs={12} md={6}>
+                                    <Select
+                                        fullWidth
+                                        variant="outlined"
+                                        idInput="idPlace"
+                                        labelInput="Cualquier lugar"
+                                        value={this.state.placeOffer}
+                                        handleChange={this.selectHandler}
+                                        name="placeOffer"
+                                        items={this.state.itemsPlace}
+                                    />
+                                </Grid>
+                            </Grid>
                         </Grid>
-                        <Grid item  xs={12} md={6}>
-                            <Select
-                                fullWidth
-                                variant="outlined"
-                                idInput="idPlace"
-                                labelInput="Cualquier lugar"
-                                value={this.state.placeOffer}
-                                handleChange={this.selectHandler}
-                                name="placeOffer"
-                                items={this.state.itemsPlace}
-                            />
+                        <Grid item xs={12} md={10}>
+                            <Offers title="" offers={this.state.offers}/>
                         </Grid>
                     </Grid>
                 </Grid>
+                <Grid item xs={10} style={{paddingBottom:"26px"}}>
+                <Facilities variant="home" />
+                </Grid>
+                <Footer/>
             </Grid>
             )
     }
 }
 
-export default Offers;
+export default OffersComponent;
