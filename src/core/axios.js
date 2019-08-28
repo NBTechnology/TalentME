be25813bff12c7cf57484aa80acc0e1bc623f52e
Sergio Redondo <redondomontes.sergio@gmail.com>
2019-08-28 10:01:37 +0200

    Add all routes to axio

--- src/core/axios.js
+++ src/core/axios.js
@@ -8,21 +8,69 @@ class axiosClient {
   }
 
   //OFFERS
-  getOffer = url => {
-    return this.client.get(url);
+
+  disableOffer = id => {
+    return this.client.get("offers/disableOffer.php", { id: id });
   };
 
-  getOffers = url => {
-    return this.client.get(url);
+  getOffer = (id) => {
+    return this.client.get("offers/getOffers.php", , { id: id });
+  };
+
+  getOffers = () => {
+    return this.client.get("offers/getOffer.php");
   };
 
   getOffersVisible = () => {
     return this.client.get("offers/getOffersOnlyVisibles.php");
   };
 
+  getOffersVisibleParams = (limit, place, job) => {
+    return this.client.get("offers/getOffersOnlyVisiblesWithParams.php", {limit: limit, place:place, job:job});
+  }
+
+  postOffer = (job, location, placesId, description, salary, durationContract, moreInfo, dateEnd, isVisible) => {
+    return this.client.get("offers/postOffer.php", {
+      job:job, 
+      location:location, 
+      placesId:placesId, 
+      description:description,
+      salary:salary, 
+      durationContract:durationContract, 
+      moreInfo:moreInfo, 
+      dateEnd:dateEnd, 
+      isVisible:isVisible});
+  }
+
+  updateOffer = (id, job, location, placesId, description, salary, durationContract, moreInfo, dateEnd, isVisible) => {
+    return this.client.get("offers/updateOffer.php", {
+      id:id,
+      job:job, 
+      location:location, 
+      placesId:placesId, 
+      description:description,
+      salary:salary, 
+      durationContract:durationContract, 
+      moreInfo:moreInfo, 
+      dateEnd:dateEnd, 
+      isVisible:isVisible});
+  }
+
   //PLACES
   getPlace = id => {
     return this.client.get("places/getPlace.php", { id: id });
+  };
+
+  getPlaces = () => {
+    return this.client.get("places/getPlaces.php");
+  };
+
+  postPlaces = (name) => {
+    return this.client.get("places/postPlace.php", {name:name});
+  };
+
+  updatePlaces = (id, name) => {
+    return this.client.get("places/postPlace.php", {id:id, name:name});
   };
 
   // get = url => {
